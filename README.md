65c29d5d0914762417eb30a74ad8f3d07ca6ed68
tbrabant3 <35499106+tbrabant3@users.noreply.github.com>
2020-03-15 17:12:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CraigScrape
+ Apartment Getter
---
+++ craigapt.py
@@ -0,0 +1,53 @@
+from flask import Flask, request
+from pymessenger.bot import Bot
+import random
+
+ACCESS_TOKEN = 'ACCESS_TOKEN'
+VERIFY_TOKEN = 'VERIFY_TOKEN'
+bot = Bot(ACCESS_TOKEN)
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    else:
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    # Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    # if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+
+
+def verify_fb_token(token_sent):
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return "Invalid verification token"
+
+
+def send_message(recipient_id, response):
+    bot.send_text_message(recipient_id, response)
+    return "Success"
+
+
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!",
+                        "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+
+if __name__ == '__main__':
+    app.run()
